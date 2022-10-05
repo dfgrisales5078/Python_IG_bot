5aaa2d9ca80785f6d36077d72208fbaee14de77b
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2022-10-05 18:05:12 -0400

    test for heroku deployment

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support import expected_conditions
-# from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Bot:
