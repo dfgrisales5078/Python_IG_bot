f1b10ce7b74c2247e95ca4d97a716abc4043338d
Diego Grisales <54412831+dfgrisales5078@users.noreply.github.com>
2022-11-30 22:41:58 -0500

    Create comment.py

--- src/comment.py
+++ src/comment.py
@@ -1,7 +1,4 @@
 def comment_on_posts(self) -> None:
-    # TODO array of comments to select a random one eveytime a comment is posted
-    comments = []
-
     comment_box_path = '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/textarea '
     comment_box = self.browser.find_element(
         By.XPATH, comment_box_path)
